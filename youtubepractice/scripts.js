e44974aa9ef0bcc0b632f21cd015a311938d94a1
Ashley Savage <cestbon@Ashleys-MacBook-Air.local>
2022-01-20 10:39:02 -0500

    practicing algorithms

--- youtubepractice/scripts.js
+++ youtubepractice/scripts.js
@@ -61,3 +61,64 @@ function reverseString2(str) {
 }
 
 console.log(reverseString2('hello'))
+
+
+function palindrome(str){
+    const revString = str.split('').reverse().join('');
+    return revString === str
+}
+
+console.log(palindrome('hello'))
+
+
+function reverseInt(int){
+    const revString = int.toString().split('').reverse().join('');
+
+    return parseInt(revString)
+}
+
+console.log(reverseInt(12345))
+
+function capitalizeLetters(str){
+    const strArr = str.toLowerCase().split(' ');
+    for(let i=0; i<strArr.length;i++){
+        strArr[i] = strArr[i].substring(0, 1).toUpperCase()+strArr[i].substring(1)
+    }
+    return strArr.join(' ')
+}
+
+console.log(capitalizeLetters('hello to you!'))
+
+
+function fizzBuzz(){
+    for(let i=1; i<=100; i++){
+        if(i%3===0 && i%5===0){
+            console.log('FizzBuzz')
+        }else if(i%3===0){
+            console.log('Fizz')
+        }else if(i%5===0){
+            console.log('Buzz')
+        }else{
+            console.log(i)
+        }
+    }
+}
+
+console.log(fizzBuzz())
+
+function reverseString3(str){
+    const strArr = str.split('').reverse().join('');
+    return strArr
+}
+
+console.log(reverseString('hello'))
+
+function reverseString5(str){
+    let revString = '';
+    for(let i=str.length-1;i>=0;i--){
+        revString = revString + str[i];
+        return revString;
+    }
+}
+
+console.log(reverseString5('hello'))
